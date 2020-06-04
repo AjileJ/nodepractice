4bae2ee88075537fcca3eb3df4da3c9388c910ae
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-06-04 16:30:03 -0700

    formating

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ server.patch('/api/hubs/:id', (req,res) => {
 const PORT = 5000;
 
 server.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+    console.log(`Listening on http://localhost:${PORT}`) ;
 });
 
 
